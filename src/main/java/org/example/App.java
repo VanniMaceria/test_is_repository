cb3849d56bb991d48929f9238cd2dd4f467bc937
VanniMaceria <andreabucci123@gamil.com>
2022-10-05 19:44:11 +0200

    Ho aggiunto prova?

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,9 +4,12 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
+public class App{
     public static void main( String[] args ){
         System.out.println("Hello Vanni Maceria!");
+        //per la branch prova
+        Persona vanni = new Persona("Vanni", "Maceria");
+        System.out.println("Hello " + vanni.toString());
+        //bla bla bla
     }
 }
